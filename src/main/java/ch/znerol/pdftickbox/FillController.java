c48a8b7e1a34258feb2705792b1603b10947ff4e
Lorenz Schori <lo@znerol.ch>
2012-12-18 21:04:02 +0100

    Show filler form

--- src/main/java/ch/znerol/pdftickbox/FillController.java
+++ src/main/java/ch/znerol/pdftickbox/FillController.java
@@ -1,29 +1,25 @@
 package ch.znerol.pdftickbox;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for the PDF filler.
  */
 @Controller
 @RequestMapping(value = "/fill")
 public class FillController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(FillController.class);
 
     /**
-     * For every request for this controller, this will 
-     * create a person instance for the form.
+     * For every request for this controller, this will create a new fill
+     * instance for the form.
      */
     @ModelAttribute
     public Fill newRequest() {
@@ -31,9 +27,20 @@ public class FillController {
     }
 
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * Return an HTML form where template and values can be submitted by the
+	 * user.
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public void home(Locale locale, Model model) {
-	}	
+	public void getForm(@ModelAttribute Fill fill, Model model) {
+		model.addAttribute("fill", fill);
+	}
+
+	/**
+	 * Return a PDF derived from the template by filling in the values provided.
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public void getPDF(@Validated Fill fill, Model model) {
+		logger.info("template: " + fill.getTemplate());
+		logger.info("values: " + fill.getValues());
+	}
 }
--- src/main/webapp/WEB-INF/views/fill.jsp
+++ src/main/webapp/WEB-INF/views/fill.jsp
@@ -1,14 +1,28 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <html>
+
 <head>
-	<title>Home</title>
+    <title>PDF Box Ticking Service</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<h2>PDF Box Ticking Service</h2>
+<form:form method="post" commandName="fill">
+
+<br/>
+
+<form:label path="template">PDF Template</form:label>
+<form:input path="template" />
+
+<br/>
+
+<form:label path="values">FDF Values</form:label>
+<form:textarea path="values" />
+
+<br/>
+
+<input type="submit" value="Get PDF" />
+
+</form:form>
 </body>
 </html>
