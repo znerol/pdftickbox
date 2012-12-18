a169fdcddab596bf03b1e828b0c142998ce450a6
Lorenz Schori <lo@znerol.ch>
2012-12-18 22:48:27 +0100

    Add stamper and dummy template

--- src/main/java/ch/znerol/pdftickbox/PDFFormFiller.java
+++ src/main/java/ch/znerol/pdftickbox/PDFFormFiller.java
@@ -1,14 +1,21 @@
 package ch.znerol.pdftickbox;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringReader;
 
 import org.springframework.stereotype.Component;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.FdfReader;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.XfdfReader;
 
 @Component
 public class PDFFormFiller implements FormFiller {
@@ -20,26 +27,47 @@ public class PDFFormFiller implements FormFiller {
 
 	@Override
 	public void fill(InputStream template, OutputStream result, String values) throws FormFillerException {
-        // step 1
-        Document document = new Document();
-        // step 2
-        try {
-			PdfWriter.getInstance(document, result);
-		} catch (DocumentException e) {
-			throw new FormFillerException("Failed to retrieve iText PdfWriter instance", e);
+		PdfReader reader;
+		try {
+			reader = new PdfReader(template);
+		} catch (IOException e) {
+			throw new FormFillerException("Failed to read PDF from template", e);
 		}
-        // step 3
-        document.open();
-        // step 4
-        try {
-			document.add(new Paragraph("Hello World!"));
-			document.add(new Paragraph(values));
-		} catch (DocumentException e) {
-			// TODO Auto-generated catch block
-			throw new FormFillerException("Failed write paragraph into document", e);
-		}
-        // step 5
-        document.close();
-	}
 
+		XfdfReader fdf;
+		try {
+			fdf = new XfdfReader(new ByteArrayInputStream(values.getBytes("UTF-8")));
+		} catch (IOException e) {
+			throw new FormFillerException("Failed create FDF reader", e);
+		}
+
+		PdfStamper stamper;
+		try {
+			stamper = new PdfStamper(reader, result);
+		} catch (DocumentException e) {
+			throw new FormFillerException("Failed create PDF stamper", e);
+		} catch (IOException e) {
+			throw new FormFillerException("Failed create PDF stamper", e);
+		}
+
+		try {
+			stamper.getAcroFields().setFields(fdf);
+		} catch (IOException e) {
+			throw new FormFillerException("Failed fill form fields", e);
+		} catch (DocumentException e) {
+			throw new FormFillerException("Failed fill form fields", e);
+		}
+
+		stamper.setFormFlattening(true);
+
+		try {
+			stamper.close();
+		} catch (DocumentException e) {
+			throw new FormFillerException("Failed to close stamper", e);
+		} catch (IOException e) {
+			throw new FormFillerException("Failed to close stamper", e);
+		}
+
+        reader.close();
+	}
 }
--- src/main/java/ch/znerol/pdftickbox/TemplateDirectory.java
+++ src/main/java/ch/znerol/pdftickbox/TemplateDirectory.java
@@ -1,7 +1,10 @@
 package ch.znerol.pdftickbox;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.log4j.lf5.util.Resource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +12,12 @@ public class TemplateDirectory implements TemplateResolver {
 
 	@Override
 	public InputStream open(String name) throws TemplateResolverException {
-		// TODO Auto-generated method stub
-		return null;
+	    ClassPathResource rsrc = new ClassPathResource("test-form.pdf");
+		try {
+			return rsrc.getInputStream();
+		} catch (IOException e) {
+			throw new TemplateResolverException("Failed to load template", e);
+		}
 	}
 
 }
